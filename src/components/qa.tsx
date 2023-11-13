3f1fe8f8f48f2a0b3ac79eb513c5635770846af2
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-11-13 12:27:19 +0100

    use #hash to make tabs linkable

--- src/components/qa.tsx
+++ src/components/qa.tsx
@@ -4,21 +4,37 @@ import { useQakuContext } from "../hooks/useQaku";
 import Question from "./question";
 import { PageDirection } from "@waku/sdk";
 import CreatePoll from "./polls/create";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Poll from "./polls/poll";
 import Polls from "./polls/poll";
+import { useLocation } from "react-router-dom";
 
 enum Tabs {
-    Questions,
-    Polls
+    Questions = "questions",
+    Polls = "polls"
 }
 
 const QA = () => {
 
     const  { controlState, localQuestions, dispatcher, isOwner } = useQakuContext()
+    const {hash} = useLocation()
     
 
     const [activeTab, setActiveTab] = useState<Tabs>(Tabs.Questions)
+
+    useEffect(() => {
+        console.log(hash)
+        switch(hash) {
+            case "#"+Tabs.Polls:
+                setActiveTab(Tabs.Polls)
+                break;
+            case "#"+Tabs.Questions:
+                setActiveTab(Tabs.Questions)
+                break
+            default:
+                setActiveTab(Tabs.Questions)
+        }
+    }, [hash])
 
     return (
         <div className="mt-5 text-center max-w-2xl m-auto">
@@ -40,8 +56,8 @@ const QA = () => {
                     <CreatePoll />
                 }
                 <div className="tabs m-auto ">
-                    <a className={`tab tab-lg tab-bordered ${activeTab == Tabs.Questions && "tab-active"}`} onClick={() => setActiveTab(Tabs.Questions)}>Questions</a> 
-                    <a className={`tab tab-lg tab-bordered ${activeTab == Tabs.Polls && "tab-active"}`} onClick={() => setActiveTab(Tabs.Polls)}>Polls</a> 
+                    <a href={`#${Tabs.Questions}`} className={`tab tab-lg tab-bordered ${activeTab == Tabs.Questions && "tab-active"}`} onClick={() => setActiveTab(Tabs.Questions)}>Questions</a>
+                    <a href={`#${Tabs.Polls}`} className={`tab tab-lg tab-bordered ${activeTab == Tabs.Polls && "tab-active"}`} onClick={() => setActiveTab(Tabs.Polls)}>Polls</a>
                 </div>
                 <div>
                 {
